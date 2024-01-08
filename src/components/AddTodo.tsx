42651ddf0e88add760a759015b1003fd66242241
Devanshi <devanshi123proton@gmail.com>
2024-01-07 22:02:00 -0500

    Addjust appearance of buttons

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -35,7 +35,7 @@ export default function AddTodo() {
       <form className="flex w-full max-w-2xl" onSubmit={handleSubmit}>
         <div className="flex-grow items-center border-b-2 border-teal-500 py-2">
           <input
-            className="appearance-none bg-transparent border-none text-gray-700 py-1 px-2 leading-tight focus:outline-none"
+            className="appearance-none bg-transparent border-none text-black-700  leading-tight focus:outline-none py-2 px-3 block w-full rounded-lg border-gray-200 bg-gray-100"
             type="text"
             id="task"
             name="task"
@@ -51,7 +51,7 @@ export default function AddTodo() {
         )}
 
         <button
-          className="bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+          className="bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded ml-3"
           type="submit"
         >
           Add Todo
